d69e6a3380192992cbf86a4405039cc845eb78c5
Fedor <fedorv1234@gmail.com>
2023-09-23 20:10:55 +0300

    Add user context

---
+++ frontend/src/context/UserContext.tsx
@@ -0,0 +1,34 @@
+import { createContext, useEffect, useState } from "react";
+
+type UserContextType = {
+  token: string | null;
+  setToken: React.Dispatch<React.SetStateAction<string | null>>;
+};
+
+export const UserContext = createContext<UserContextType | null>(null);
+
+export const UserProvider = (props: any) => {
+  const [token, setToken] = useState(localStorage.getItem("authToken"));
+  /*Check the token */
+  useEffect(() => {
+    const fetchUser = async () => {
+      const requestParams = {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: "Bearer " + token,
+        },
+      };
+      const response = await fetch("/api/auth", requestParams);
+      if (!response.ok) {
+        setToken(null);
+      } else localStorage.setItem("authToken", token ?? "null");
+    };
+    fetchUser();
+  }, [token]);
+  return (
+    <UserContext.Provider value={{ token, setToken }}>
+      {props.children}
+    </UserContext.Provider>
+  );
+};
