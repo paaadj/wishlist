072f59005dcacc32934031c557cefb51b518428a
Korablev Mikhail <smroc8@gmail.com>
2023-10-18 22:34:16 +0300

    added tests for registration

---
+++ backend/conftest.py
@@ -0,0 +1,53 @@
+"""
+Test config
+"""
+
+import pytest
+from httpx import AsyncClient
+from tortoise import Tortoise
+from main import app
+
+
+DB_URL = "sqlite://:memory:"
+
+
+async def init_db(db_url, create_db: bool = False, schemas: bool = False) -> None:
+    """Initial database connection"""
+    await Tortoise.init(
+        db_url=db_url, modules={"models": ["models.item", "models.user"]}
+    )
+    if create_db:
+        print(f"Database created! {db_url = }")
+    if schemas:
+        await Tortoise.generate_schemas()
+        print("Success to generate schemas")
+
+
+# pylint: disable=missing-function-docstring
+async def init(db_url: str = DB_URL):
+    await init_db(db_url, True, True)
+
+
+# pylint: disable=missing-function-docstring
+@pytest.fixture(scope="session")
+def anyio_backend():
+    return "asyncio"
+
+
+# pylint: disable=missing-function-docstring
+@pytest.fixture(scope="session")
+async def client():
+    async with AsyncClient(app=app, base_url="http://127.0.0.1:8000/api") as client_inside:
+        print("Client is ready")
+        yield client_inside
+
+
+# pylint: disable=missing-function-docstring, protected-access
+@pytest.fixture(scope="session", autouse=True)
+async def initialize_tests():
+    await init()
+    yield
+    # for model in Tortoise.apps.get('models').keys():
+    #     query = DeleteQuery(model=model)
+    #     await query.delete()
+    await Tortoise._drop_databases()
---
+++ backend/tests/test_main.py
@@ -0,0 +1,209 @@
+"""
+Module containing tests
+"""
+
+import pytest
+from httpx import AsyncClient
+
+from models.user import User
+
+
+# /register testing
+@pytest.mark.anyio
+async def test_testpost(client: AsyncClient):
+    """
+    user registration test
+    """
+    await User.all().delete()
+    username, email, password = ["mihaTest", "mihaTest@mail.ru", "bibis"]
+    assert await User.filter(username=username).count() == 0
+    data = {"username": username, "email": email, "password": password}
+    response = await client.post("/register", json=data)
+    data.pop("password")
+    assert response.json() == dict(data)
+    assert response.status_code == 200
+
+    assert await User.filter(username=username).count() == 1
+
+
+@pytest.mark.anyio
+async def test_register_empty_email(client: AsyncClient):
+    """
+        user registration test
+    """
+    await User.all().delete()
+    username, email, password = ["mihaTest", "", "bibis"]
+    assert await User.filter(username=username).count() == 0
+
+    data = {"username": username, "email": email, "password": password}
+    response = await client.post("/register", json=data)
+
+    assert 400 <= response.status_code < 500
+    assert await User.filter(username=username).count() == 0
+
+
+@pytest.mark.anyio
+async def test_register_empty_pass(client: AsyncClient):
+    """
+        user registration test
+    """
+    await User.all().delete()
+    username, email, password = ["mihaTest", "mihaTest@mail.ru", ""]
+    assert await User.filter(username=username).count() == 0
+
+    data = {"username": username, "email": email, "password": password}
+    response = await client.post("/register", json=data)
+
+    assert 400 <= response.status_code < 500
+    assert await User.filter(username=username).count() == 0
+
+
+@pytest.mark.anyio
+async def test_register_empty_username(client: AsyncClient):
+    """
+        user registration test
+    """
+    await User.all().delete()
+    username, email, password = ["", "mihaTest@mail.ru", "bibis"]
+    assert await User.filter(username=username).count() == 0
+
+    data = {"username": username, "email": email, "password": password}
+    response = await client.post("/register", json=data)
+
+    assert 400 <= response.status_code < 500
+    assert await User.filter(username=username).count() == 0
+
+
+@pytest.mark.anyio
+async def test_register_existing_user(client: AsyncClient):
+    """
+        user registration test
+    """
+    await User.all().delete()
+    username, email, password = ["mihaTest", "mihaTest@mail.ru", "bibis"]
+    assert await User.filter(username=username).count() == 0
+
+    data = {"username": username, "email": email, "password": password}
+    await client.post("/register", json=data)
+    response = await client.post("/register", json=data)
+
+    assert await User.filter(username=username).count() == 1
+    assert 400 <= response.status_code < 500
+
+
+@pytest.mark.anyio
+async def test_register_existing_user_with_diff_pass(client: AsyncClient):
+    """
+        user registration test
+    """
+    await User.all().delete()
+    username, email, password = ["mihaTest", "mihaTest@mail.ru", "bibis"]
+    assert await User.filter(username=username).count() == 0
+
+    data = {"username": username, "email": email, "password": password}
+    await client.post("/register", json=data)
+    data = {"username": username, "email": email, "password": "nebibis"}
+    response = await client.post("/register", json=data)
+
+    assert await User.filter(username=username).count() == 1
+    assert 400 <= response.status_code < 500
+
+
+@pytest.mark.anyio
+async def test_register_existing_user_with_diff_email(client: AsyncClient):
+    """
+        user registration test
+    """
+    await User.all().delete()
+    username, email, password = ["mihaTest", "mihaTest@mail.ru", "bibis"]
+    assert await User.filter(username=username).count() == 0
+
+    data = {"username": username, "email": email, "password": password}
+    await client.post("/register", json=data)
+    data = {"username": username, "email": "ChrisWilson@ggg.com", "password": "bibis"}
+    response = await client.post("/register", json=data)
+
+    assert await User.filter(username=username).count() == 1
+    assert 400 <= response.status_code < 500
+
+
+@pytest.mark.anyio
+async def test_register_same_email(client: AsyncClient):
+    """
+        user registration test
+    """
+    await User.all().delete()
+    username, email, password = ["mihaTest", "mihaTest@mail.ru", "bibis"]
+    assert await User.filter(username=username).count() == 0
+
+    data = {"username": username, "email": email, "password": password}
+    await client.post("/register", json=data)
+    data = {
+        "username": "ChrisWilson",
+        "email": "mihaTest@mail.ru",
+        "password": "nebibis",
+    }
+    response = await client.post("/register", json=data)
+
+    assert await User.filter(username=username).count() == 1
+    assert await User.filter(username="ChrisWilson").count() == 0
+    assert 400 <= response.status_code < 500
+
+
+# /token testing
+@pytest.mark.anyio
+async def test_get_token_for_existing_user(client: AsyncClient):
+    """
+        user registration test
+    """
+    await User.all().delete()
+    username, email, password = ["mihaTest", "mihaTest@mail.ru", "bibis"]
+    data_for_registration = {"username": username, "email": email, "password": password}
+    data_for_token = {"username": username, "password": password}
+    await client.post("/register", json=data_for_registration)
+    response = await client.post("/token", data=data_for_token)
+    assert response.status_code == 200
+
+
+@pytest.mark.anyio
+async def test_get_token_for_non_existing_user(client: AsyncClient):
+    """
+        user registration test
+    """
+    await User.all().delete()
+    username, password = ["mihaTest",  "bibis"]
+    data_for_token = {"username": username, "password": password}
+    response = await client.post("/token", data=data_for_token)
+    assert 400 <= response.status_code < 500
+
+
+@pytest.mark.anyio
+async def test_get_token_for_wrong_password(client: AsyncClient):
+    """
+        user registration test
+    """
+    await User.all().delete()
+    username, email, password = ["mihaTest", "mihaTest@mail.ru", "bibis"]
+    data_for_token = {"username": username, "password": "nebibis"}
+    data_for_registration = {"username": username, "email": email, "password": password}
+    await client.post("/register", json=data_for_registration)
+    response = await client.post("/token", data=data_for_token)
+    assert 400 <= response.status_code < 500
+
+
+# get user testing
+@pytest.mark.anyio
+async def test_get_me(client: AsyncClient):
+    """
+        get user test
+    """
+    await User.all().delete()
+    username, email, password = ["mihaTest", "mihaTest@mail.ru", "bibis"]
+    data_for_registration = {"username": username, "email": email, "password": password}
+    data_for_token = {"username": username, "password": password}
+    await client.post("/register", json=data_for_registration)
+    response_token = await client.post("/token", data=data_for_token)
+    get_headers = {"Authorization": f"Bearer {response_token.json()['access_token']}"}
+    response_get = await client.get("/users/me", headers=get_headers)
+    assert response_get.status_code == 200
+    assert response_get.json()["username"] == "mihaTest"
