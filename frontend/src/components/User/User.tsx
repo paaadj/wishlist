6d89943530b9c9fbbc7d1ec31cb805a6e1a59a1c
Fedor <fedorv1234@gmail.com>
2023-10-30 23:27:06 +0300

    fix(build): fix build errors

--- frontend/src/components/User/User.tsx
+++ frontend/src/components/User/User.tsx
@@ -1,13 +1,7 @@
-import { useContext, useEffect, useState } from "react";
+import { useContext, useEffect } from "react";
 import { UserContext, UserContextType } from "../../context/UserContext";
 import { useNavigate } from "react-router-dom";
 
-type userData = {
-  firstName: string,
-  lastName?: string,
-  username: string,
-  email: string
-};
 
 function User() {
   const navigate = useNavigate();
@@ -15,10 +9,6 @@ function User() {
   const { user, setAuthorizationTokens, isAuthenticated } =
     useContext(UserContext) as UserContextType;
   
-  useEffect(() => {
-    console.log(user);
-  }, []);
-
   return (
     <>
       <h1>Nice auth, {user ? user.username : "Loading"}</h1>
