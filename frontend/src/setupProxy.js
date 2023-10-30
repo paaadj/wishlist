e2cefbba7bb4c9f009086839e621b30f774aa2f2
Fedor <fedorv1234@gmail.com>
2023-10-31 01:01:18 +0300

    fix(docker): fix docker communication

--- frontend/src/setupProxy.js
+++ frontend/src/setupProxy.js
@@ -1,10 +1,12 @@
 const { createProxyMiddleware } = require("http-proxy-middleware");
 
+const dom = process.env.REACT_APP_DOMAIN ? process.env.REACT_APP_DOMAIN : "backend";
+
 module.exports = function (app) {
   app.use(
     '/backend',
     createProxyMiddleware({
-      target: "http://127.0.0.1:8000",
+      target: `http://${dom}:8000`,
       changeOrigin: true,
       pathRewrite: function (path, req) { return path.replace('/backend', '') }
     })
