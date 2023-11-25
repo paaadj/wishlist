c24876017ca1c5345d64878d0e2e5ca607898ed4
Vasiliy Ivanov <paaadjj@gmail.com>
2023-11-25 14:15:48 +0300

    Changed websocket auth method

--- backend/api/chat_routes.py
+++ backend/api/chat_routes.py
@@ -1,6 +1,6 @@
 import json
-from typing import Dict
-from fastapi import APIRouter, WebSocket, HTTPException, status, Depends
+from typing import Dict, Annotated
+from fastapi import APIRouter, WebSocket, HTTPException, status, Depends, Query
 from fastapi.security import OAuth2PasswordBearer
 
 from auth.services import get_current_user
@@ -15,27 +15,22 @@ chat_router = APIRouter()
 connections: Dict[int, set] = {}
 
 
-async def get_user_ws(websocket: WebSocket) -> User:
-    token = websocket.headers.get('Authorization')
-    if token is None:
-        await websocket.close(reason="Unauthorized")
-    return await get_current_user(token)
-
-
 @chat_router.websocket("/chats/{chat_id}/ws")
 async def websocket_endpoint(
         websocket: WebSocket,
         chat_id: int,
+        token=Annotated[str | None, Query()],
 ):
     await websocket.accept()
     chat = await Chat.exists(id=chat_id)
     if not chat:
-        await websocket.close(reason=f"Chat with id {chat_id} is not exists")
+        await websocket.close(code=status.WS_1008_POLICY_VIOLATION ,reason=f"Chat with id {chat_id} is not exists")
     if chat_id not in connections:
         connections[chat_id] = set()
-    user = await get_user_ws(websocket)
-    websocket.current_user = user
     connections[chat_id].add(websocket)
+    user: User = None
+    if token:
+        user = await get_current_user(token)
     try:
         while True:
             data = await websocket.receive_text()
@@ -45,10 +40,12 @@ async def websocket_endpoint(
                 reply_to = json.loads(data)["reply_to"]
             except KeyError:
                 pass
-            user = websocket.current_user
+
             message = await send_message(text=text, chat_id=chat_id, user=user, reply_to=reply_to)
             for conn in connections[chat_id]:
                 if conn.client_state != 3:
                     await conn.send_text(message.__dict__.__str__())
-    except Exception as exc:
-        await websocket.close(reason=f"{exc}")
+    except KeyError as exc:
+        await websocket.close(code=status.WS_1003_UNSUPPORTED_DATA, reason=f"required {exc}, but nothing was received")
+    except:
+        await websocket.close(code=status.WS_1006_ABNORMAL_CLOSURE, reason="Unknown error")
